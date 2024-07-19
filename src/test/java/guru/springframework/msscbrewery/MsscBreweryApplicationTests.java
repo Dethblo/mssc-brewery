cb96e8298d68b2d2f5673f3a3b0db56fbc0909d9
Howard Burgett <hburgett@researchinnovations.com>
2024-07-19 17:51:53 -0400

    Added a test whose only purpose is to display BeerDto JSON.

--- src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
+++ src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -1,13 +1,29 @@
 package guru.springframework.msscbrewery;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MsscBreweryApplicationTests {
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void beerDto() throws JsonProcessingException {
+		BeerDto beerDto = BeerDto.builder().id(UUID.randomUUID())
+				.beerName("Galaxy Cat")
+				.beerStyle("Pale Ale")
+				.build();
+		String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(beerDto);
+		System.out.println(json);
+	}
 }
